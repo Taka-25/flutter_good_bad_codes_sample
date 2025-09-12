9b0a0b6aba5c3c0dd74d4ac89d6e24bdd98ef4bf
Taka-025 <taka25901@gmail.com>
2025-09-12 23:26:15 +0900

    第5章 bad途中

---
+++ lib/chapter/5_low_cohesion/bad/chaper5_bad.dart
@@ -0,0 +1,78 @@
+// 5.1
+// 注文を管理するクラス
+// ignore_for_file: non_constant_identifier_names, slash_for_doc_comments
+
+class OrderManager {
+  static int add(int moneyAmount1, int moneyAmount2) {
+    return moneyAmount1 + moneyAmount2;
+  }
+}
+
+// 5.1.3
+class PaymentManager {
+  int _discountRate;
+
+  PaymentManager(this._discountRate);
+
+  int add(int moneyAmount1, int moneyAmount2) {
+    return moneyAmount1 + moneyAmount2;
+  }
+}
+
+// 5.2
+class GiftPoint {
+  static final int _MIN_POINT = 0;
+  final int value;
+
+  GiftPoint(this.value) {
+    if (value < _MIN_POINT) {
+      throw ArgumentError('ポイントが0以上ではありません。');
+    }
+  }
+
+  /** 
+   * ポイントを加算する。
+   * 
+   * @param other 加算ポイント
+   * @return 加算後の残余ポイント
+   */
+  GiftPoint add(final GiftPoint other) {
+    return GiftPoint(value + other.value);
+  }
+
+  /**
+   * @return 残余ポイントが消費ポイント以上であればtrue
+   */
+  bool isEnough(final ConsumptionPoint point) {
+    return point.value <= value;
+  }
+
+  /** 
+   * ポイントを減算する。
+   * 
+   * @param point 消費ポイント
+   * @return 消費後の残余ポイント
+   */
+  GiftPoint consume(final ConsumptionPoint point) {
+    if (!isEnough(point)) {
+      throw ArgumentError('ポイントが不足しています。');
+    }
+
+    return GiftPoint(value - point.value);
+  }
+}
+
+class ConsumptionPoint {
+  static final int _MIN_POINT = 0;
+  final int value;
+
+  ConsumptionPoint(this.value) {
+    if (value < _MIN_POINT) {
+      throw ArgumentError('ポイントが0以上ではありません。');
+    }
+  }
+}
+
+// 5.2
+
+void main() {}
