9eb5a0db0d1e1f94609a2ed85f17ebe86eb001dc
Taka-025 <taka25901@gmail.com>
2025-09-29 10:19:23 +0900

    importパッケージをdart:mathに変更

--- lib/chapter/5_low_cohesion/good/chapter5_good.dart
+++ lib/chapter/5_low_cohesion/good/chapter5_good.dart
@@ -1,12 +1,11 @@
 // ignore_for_file: non_constant_identifier_names, unused_element, slash_for_doc_comments, constant_identifier_names, unused_field
+import 'dart:math';
+
+import 'package:flutter_good_bad_codes_sample/src/utils/logger/logger.dart';
 
 // 5.7
 // Good: コンストラクタがprivateで、目的別のファクトリメソッドを用意している
 //コンストラクタが非公開であることで、関連ロジックの分散を防いでいる
-import 'dart:nativewrappers/_internal/vm/lib/math_patch.dart';
-
-import 'package:flutter_good_bad_codes_sample/src/utils/logger/logger.dart';
-
 class _GiftPoint {
   static final int _MIN_POINT = 0;
   static final int STANDARD_MEMBERSHIP_POINT = 3000;
