6b1edacc030a976be481b367cf6b645a7e770b8a
Taka-025 <taka25901@gmail.com>
2025-10-05 23:19:00 +0900

    6.1

---
+++ lib/chapter/6_condition/chapter6.dart
@@ -0,0 +1,38 @@
+// 6.1
+// Bad: if文で多重にネストした構造
+// ネストしすぎて可読性が下がっている。保守性も下がる
+class BadMagicAttack {
+  final BadMember badMember;
+  final BadMagic badMagic;
+  BadMagicAttack(this.badMember, this.badMagic);
+
+  void magicAttack() {
+    if (0 < badMember.hitPoint) {
+      if (badMember.canAct) {
+        if (badMagic.costMagicPoint <= badMember.magicPoint) {
+          badMember.consumerMagicPoint(badMember.magicPoint);
+          badMember.chant(badMagic);
+        }
+      }
+    }
+  }
+}
+
+// 6.1用クラス
+class BadMember {
+  final int hitPoint;
+  final bool canAct;
+  final int magicPoint;
+  BadMember(this.hitPoint, this.canAct, this.magicPoint);
+
+  // 6.1用メソッド
+  void consumerMagicPoint(int magicPoint) {}
+
+  // 6.1用メソッド
+  void chant(BadMagic magic) {}
+}
+
+class BadMagic {
+  final int costMagicPoint;
+  BadMagic(this.costMagicPoint);
+}
