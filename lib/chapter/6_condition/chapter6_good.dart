65157ac7a5a0bc396d93983e88abaaaa0547f315
Taka-025 <taka25901@gmail.com>
2025-10-06 08:15:07 +0900

    6.3

--- lib/chapter/6_condition/chapter6_good.dart
+++ lib/chapter/6_condition/chapter6_good.dart
@@ -1,1 +1,51 @@
+// 6.3
+// Good: 早期returnでネスト解消している
+// これにより可読性・保守性が上がっている
+class MagicAttack {
+  final Member member;
+  final Magic magic;
+  MagicAttack(this.member, this.magic);
 
+  /// 6.1（badサンプル）
+  // void magicAttack() {
+  //   if (0 < member.hitPoint) {
+  //     if (member.canAct) {
+  //       if (magic.costMagicPoint <= member.magicPoint) {
+  //         member.consumerMagicPoint(member.magicPoint);
+  //         member.chant(magic);
+  //       }
+  //     }
+  //   }
+  // }
+
+  void magicAttack() {
+    if (member.hitPoint <= 0) return;
+
+    if (member.canAct) {
+      if (magic.costMagicPoint <= member.magicPoint) {
+        member.consumerMagicPoint(magic.costMagicPoint);
+        member.chant(magic);
+      }
+    }
+  }
+}
+
+// 6.3用クラス
+class Member {
+  final int hitPoint;
+  final bool canAct;
+  final int magicPoint;
+  Member(this.hitPoint, this.canAct, this.magicPoint);
+
+  // 6.3用メソッド
+  void consumerMagicPoint(int magicPoint) {}
+
+  // 6.3用メソッド
+  void chant(Magic magic) {}
+}
+
+// 6.3用クラス
+class Magic {
+  final int costMagicPoint;
+  Magic(this.costMagicPoint);
+}
